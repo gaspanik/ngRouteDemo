f1c35a11a3c76ea10ddd8d378d55341a823cc838
cipher <gmgateway@gmail.com>
2014-09-13 23:10:30 +0900

    Minor update

--- scripts.js
+++ scripts.js
@@ -1,44 +1,49 @@
-var app = angular.module('myApp', ['ngRoute']);
+(function() {
+  'use strict';
 
-app.config(function($routeProvider, $locationProvider) {
-  $locationProvider.hashPrefix('!');
-  // $locationProvider.html5Mode(true);
+  var app = angular.module('myApp', ['ngRoute']);
 
-  $routeProvider
-    .when('/', {
-      templateUrl: 'pages/home.html',
-      controller: 'mainController'
-    })
-    .when('/about', {
-      templateUrl: 'pages/about.html',
-      controller: 'aboutController'
-    })
-    .when('/contact', {
-      templateUrl: 'pages/contact.html',
-      controller: 'contactController'
-    });
-});
+  app.config(function($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('!');
+    // $locationProvider.html5Mode(true);
 
-app.controller('mainController', function($scope) {
-  $scope.message = 'Everyone come and see my AngularJS.';
-  $scope.$parent.cond = {
-    pageTitle: '',
-    pageDesc: 'Hello my AngularJS.'
-  }
-});
+    $routeProvider
+      .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'mainController'
+      })
+      .when('/about', {
+        templateUrl: 'pages/about.html',
+        controller: 'aboutController'
+      })
+      .when('/contact', {
+        templateUrl: 'pages/contact.html',
+        controller: 'contactController'
+      });
+  });
 
-app.controller('aboutController', function($scope) {
-  $scope.message = 'Hello, I am About Page.'
-  $scope.$parent.cond = {
-    pageTitle: '| about',
-    pageDesc: 'Hello, About page.'
-  }
-});
+  app.controller('mainController', function($scope) {
+    $scope.message = 'Everyone come and see my AngularJS.';
+    $scope.$parent.cond = {
+      pageTitle: '',
+      pageDesc: 'Hello my Angular.'
+    };
+  });
 
-app.controller('contactController', function($scope) {
-  $scope.message = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.';
-  $scope.$parent.cond = {
-    pageTitle: '| contact',
-    pageDesc: 'Contact me, Hahaha.'
-  }
-});
+  app.controller('aboutController', function($scope) {
+    $scope.message = 'Hello, I am About Page.';
+    $scope.$parent.cond = {
+      pageTitle: '| About',
+      pageDesc: 'Hello about page.'
+    };
+  });
+
+  app.controller('contactController', function($scope) {
+    $scope.message = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.';
+    $scope.$parent.cond = {
+      pageTitle: '| Contact',
+      pageDesc: 'Contact me.'
+    };
+  });
+
+})();
